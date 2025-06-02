e7da8a20da1cfcd5674215502ccbde144968f102
Basava <159115403+MS4534@users.noreply.github.com>
2025-06-02 19:56:11 +0530

    Update 9 cancer patient .py

--- 9 cancer patient .py
+++ 9 cancer patient .py
@@ -1,36 +1,24 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sb
-from sklearn.decomposition import PCA
+import seaborn as sns
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
-
-df=pd.read_csv("/content/cancer patient data sets.csv")
-df
-
-df.isnull()
-
-df.isnull().sum()
-
-data=df.drop(columns=['Fatigue'])
-data=data.drop(columns=['Dust Allergy'])
-data.dropna(inplace=True)
-X=data.drop(columns=['Patient Id', 'Level'])
-scaler=StandardScaler()
-X_scaled=scaler.fit_transform(X)
-
-Kmeans=KMeans(n_clusters=2,random_state=42,n_init=10)
-Kmeans.fit(X_scaled)
-
-data['cluster']=Kmeans.labels_
-
-plt.figure(figsize=(8,6))
-plt.scatter(X_scaled[:,0],X_scaled[:,1],c=Kmeans.labels_,cmap='viridis',s=50,alpha=0.7)
-plt.title('K-Means Clustering on Cancer Patient Data',fontsize=14)
-plt.xlabel('Feature 1',fontsize=12)
-plt.xlabel('Feature 2',fontsize=12)
+df = pd.read_csv("Breast_Cancer.csv") 
+df.drop(columns=['id'], inplace=True) 
+df.drop(columns=['Unnamed: 32'], inplace=True)
+df.dropna(inplace=True)
+X = df.drop(columns=['diagnosis'])
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+kmeans = KMeans(n_clusters=2, random_state=42)
+kmeans.fit(X_scaled)
+df['Cluster'] = kmeans.labels_
+plt.figure(figsize=(8, 6))
+plt.scatter(X_scaled[:, 0], X_scaled[:, 1], c=kmeans.labels_, cmap='viridis', s=50, alpha=0.7)
+plt.title('K-Means Clustering on Wisconsin Breast Cancer Data', fontsize=14)
+plt.xlabel('Feature 1', fontsize=12)
+plt.ylabel('Feature 2', fontsize=12)
 plt.show()
-
-print(f'Inertia(Within-Cluster Sum of Squuared Distances):{Kmeans.inertia_}')
