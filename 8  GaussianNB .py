af8eac193cebb8cb6ba0a478db74ca9aa45ff01d
Basava <159115403+MS4534@users.noreply.github.com>
2025-06-02 19:42:02 +0530

    Update 8  GaussianNB .py

--- 8  GaussianNB .py
+++ 8  GaussianNB .py
@@ -1,7 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=40)
-
+from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_iris
+df=pd.read_csv("iris.data.csv")
+iris=load_iris()
+x=iris.data
+y=iris.target
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2, random_state=42)
 model=GaussianNB()
 model.fit(x_train,y_train)
-y_pred=model.predict(x_test)                                                                                                                                                             accuracy=accuracy_score(y_test,y_pred)
+y_pred=model.predict(x_test)   
+accuracy=accuracy_score(y_test,y_pred)
 print(f"Accuracy of Naive Bayes Model :{accuracy:.2f}")
